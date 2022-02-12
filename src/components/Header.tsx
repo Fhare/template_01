e875bb405cc066a4f70daecbc37fab5d9c7ee22e
Yago Rodrigues <yago.yaguinho@live.com>
2022-02-12 17:49:10 -0300

    Update all UI of project

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,20 @@
+import { HiOutlineSearch } from "react-icons/hi";
+
 import "../styles/Component.Header.scss";
 
 export function Header() {
   return (
     <div className="header">
-      <ul>
-        <li><a>Início</a></li>
-        <li><a>Produtos</a></li>
-        <li><a>Clientes</a></li>
-        <li><a>Contato</a></li>
-      </ul>
-
-      <div>
-        <h2>Logo</h2>
-      </div>
-
-      <div className="buttons">
-        <button>Entrar</button>
-        <button>Contratar serviços</button>
-      </div>
+      <nav>
+        <ul>
+          <li><a>iPhone</a></li>
+          <li><a>Samsung</a></li>
+          <li><a>Xiaomi</a></li>
+          <li><a>Motorola</a></li>
+          <li><a>LG</a></li>
+          <li><a><HiOutlineSearch style={{ marginTop: "5px" }} size={15} /></a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
